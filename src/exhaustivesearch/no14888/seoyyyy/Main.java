8b568f40f2446ed861f9a70a41327702acc1a7a9
seoyyyy <seoyyyy@naver.com>
2022-04-17 19:29:01 +0900

    no.14888

--- src/exhaustivesearch/no14888/seoyyyy/Main.java
+++ src/exhaustivesearch/no14888/seoyyyy/Main.java
@@ -6,31 +6,72 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int [] numbers;
+    static int[] arith;
+    static int MAX = Integer.MIN_VALUE;
+    static int MIN = Integer.MAX_VALUE;
+
     public void solution() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
+        numbers = new int[n];
         StringTokenizer stk = new StringTokenizer(br.readLine());
-
-        int[] arr = new int[n];
-        int i = 0;
-        while (stk.hasMoreTokens()){
-            arr[i] = Integer.parseInt(stk.nextToken());
-            i ++;
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = Integer.parseInt(stk.nextToken());
         }
 
-        int[] arith = new int[4];
-        i = 0;
         stk = new StringTokenizer(br.readLine());
-        while(stk.hasMoreTokens()){
+        arith = new int[4];
+
+        for (int i = 0; i < arith.length; i++) {
             arith[i] = Integer.parseInt(stk.nextToken());
-            i ++;
+
         }
 
+        calculate(numbers[0], 1);
+        System.out.printf("%d\n%d", MAX, MIN);
     }
 
-    public static void main(String[] args) throws IOException{
-        Main m = new Main();
-        m.solution();
+    public static void calculate(int calNum, int idx){
+        if(idx == numbers.length){
+            MAX = Math.max(MAX, calNum);
+            MIN = Math.min(MIN, calNum);
+           return;
+        }
+
+        for (int i = 0; i < arith.length; i++) {
+            if(arith[i] > 0){
+                arith[i] --;
+
+                switch (i){
+                    case 0:
+                        calculate(calNum + numbers[idx], idx + 1);
+                        break;
+                    case 1:
+                        calculate(calNum - numbers[idx], idx + 1);
+                        break;
+                    case 2:
+                        calculate(calNum * numbers[idx], idx + 1);
+                        break;
+                    case 3:
+                        calculate(calNum / numbers[idx], idx + 1);
+                        break;
+                }
+                arith[i] ++;
+            }
+
+        }
+    }
+
+
+    public static void main(String[] args) {
+        try {
+            Main m = new Main();
+            m.solution();
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
 
 }
