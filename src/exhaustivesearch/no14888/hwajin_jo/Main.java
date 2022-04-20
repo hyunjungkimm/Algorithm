2f7dfaee2a0da400111b1ebf205d0b58317a3d71
hwajin-jo <wa8784@naver.com>
2022-04-20 17:22:57 +0900

    20220420

--- src/exhaustivesearch/no14888/hwajin_jo/Main.java
+++ src/exhaustivesearch/no14888/hwajin_jo/Main.java
@@ -3,13 +3,18 @@ package exhaustivesearch.no14888.hwajin_jo;
 import java.util.Scanner;
 
 public class Main {
+    static int N; // 숫자 갯수
+    static int[] arr; // 숫자
+    static int[] operator; // 연산자
+    static int min = Integer.MAX_VALUE; // 최솟값
+    static int max = Integer.MIN_VALUE; // 최댓값
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int N = sc.nextInt();
-        int[] arr = new int[N];
-        int[] operator = new int[4];
+        N = sc.nextInt();
+        arr = new int[N];
+        operator = new int[4];
 
         for (int i = 0; i < N; i++) {
             arr[i] = sc.nextInt();
@@ -19,8 +24,36 @@ public class Main {
             operator[i] = sc.nextInt();
         }
 
-        switch (operator[N]) {
-            case 0:
+        solution(arr[0], 1);
+
+        System.out.println(max);
+        System.out.println(min);
+
+    }
+
+    public static void solution(int num, int depth) {
+
+        if (depth == N) {
+            max = Math.max(max, num);
+            min = Math.min(min, num);
+        }
+
+        for (int i = 0; i < 4; i++) {
+            if (operator[i] > 0) {
+                operator[i]--;
+                switch(i) {
+                    case 0 : solution(num + arr[depth], depth + 1);
+                        break;
+                    case 1: solution(num - arr[depth], depth + 1);
+                        break;
+                    case 2: solution(num * arr[depth], depth + 1);
+                        break;
+                    case 3: solution(num / arr[depth], depth + 1);
+                        break;
+                }
+
+                operator[i]++;
+            }
 
         }
     }
--- src/exhaustivesearch/no15649/hwajin_jo/Main.java
+++ src/exhaustivesearch/no15649/hwajin_jo/Main.java
@@ -16,25 +16,26 @@ public class Main {
         M = sc.nextInt();
 
         arr = new int[M];
-        visited = new boolean[N];
+        visited = new boolean[N + 1];
 
         solution(0);
+
         System.out.println(sb);
     }
 
     public static void solution(int depth) {
 
         if (depth == M) {
-            for (int val : arr) {
-                sb.append(val).append(' ');
+            for (int i = 0; i < M; i++) {
+                sb.append(arr[i]).append(' ');
             }
             sb.append('\n');
         } else {
             for (int i = 0; i < N; i++) {
-                if (!visited[i]) { // 만약 해당 노드를 아직 방문하지 않았다면
-                    visited[i] = true; // 해당 노드를 방문처리 한다.
-                    arr[depth] = i + 1; // 해당 깊이를 인덱스로 하여 i값 저장
-                    solution(depth + 1); // 다음 자식 노드 방문을 위해 depth + 1을 재귀호출
+                if (!visited[i]) {
+                    visited[i] = true;
+                    arr[depth] = i + 1;
+                    solution(depth + 1);
 
                     visited[i] = false;
                 }
