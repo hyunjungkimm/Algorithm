efb6427ac4c443c49f4cedb7e1d7d6cf258743fe
hyunjungkimm <97015607+hyunjungkimm@users.noreply.github.com>
2022-04-29 20:07:07 +0900

    Update Main.java

--- src/sorting/no10825/hyungjungkimm/Main.java
+++ src/sorting/no10825/hyungjungkimm/Main.java
@@ -1,4 +1,86 @@
 package sorting.no10825.hyungjungkimm;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
 public class Main {
+    static int N;
+    static String[][] students;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+
+        students = new String[N][4];
+
+        for(int i = 0; i<N; i++){
+            for(int j = 0; j<4; j++){
+                students[i][j] = sc.next();
+            }
+        }
+        Arrays.sort(students, new Comparator<String[]>() {
+            @Override
+            public int compare(String[] o1, String[] o2) {
+
+                if (Integer.parseInt(o1[1]) == Integer.parseInt(o2[1])) {
+                    if (Integer.parseInt(o1[2]) == Integer.parseInt(o2[2])) {
+                        if (Integer.parseInt(o1[3]) == Integer.parseInt(o2[3])) {
+                            return o1[0].compareTo(o2[0]);
+                        }
+                        return Integer.parseInt(o2[3]) - Integer.parseInt(o1[3]);
+                    }
+                    return Integer.parseInt(o1[2]) - Integer.parseInt(o2[2]);
+                }else{
+                    return Integer.parseInt(o2[1]) - Integer.parseInt(o1[1]);
+                }
+
+            }
+        });
+   /*    Arrays.sort(students, new Comparator<String[]>() {
+            @Override
+            public int compare(String[] o1, String[] o2) {
+
+                if (o1[1].equals(o2[1])){
+                    if(o1[2].equals(o2[2])) {
+                        if (o1[3].equals(o2[3])) {
+                            return o1[0].compareTo(o2[0]);
+                        }
+                        return Integer.parseInt(o2[3]) - Integer.parseInt(o1[3]);
+                    }
+                    return Integer.parseInt(o1[2]) - Integer.parseInt(o2[2]);
+                }else{
+                    return Integer.parseInt(o2[1]) - Integer.parseInt(o1[1]);
+                }
+
+            }
+        });*/
+        for(int i = 0; i<N; i++) {
+            System.out.println(students[i][0]);
+        }
+    }
 }
+
+
+ /*   Arrays.sort(students, new Comparator<String[]>() {
+            @Override
+            public int compare(String[] s1, String[] s2) {
+                if (Integer.parseInt(s1[1]) == Integer.parseInt(s2[1])) {
+                    if (Integer.parseInt(s1[2]) == Integer.parseInt(s2[2])) {
+                        if (Integer.parseInt(s1[3]) == Integer.parseInt(s2[3])) {
+
+                            //국영수 점수가 같다면 사전 오름차순
+                            return s1[0].compareTo(s2[0]);
+                        }
+
+                        //국어점수 같고 영어점수 같을 때, 수학 점수는 내림차순
+                        return Integer.compare(Integer.parseInt(s2[3]), Integer.parseInt(s1[3]));
+
+                    }
+                    //국어 점수 같을 때, 영어 점수는 오름차순
+                    return Integer.compare(Integer.parseInt(s1[2]), Integer.parseInt(s2[2]));
+                }
+                //국어점수는 내림차순
+                return Integer.compare(Integer.parseInt(s2[1]), Integer.parseInt(s1[1]));
+            }
+
+        });*/
